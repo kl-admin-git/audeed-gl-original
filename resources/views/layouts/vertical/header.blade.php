2e59b8d4c0ad3f7102f688a122d90bdbc5b325c0
Roberto jose arenas mazuera <rjam480@gmail.com>
2026-09-01 13:21:04 -0500

    se agregan los id para consulta

--- resources/views/layouts/vertical/header.blade.php
+++ resources/views/layouts/vertical/header.blade.php
@@ -91,7 +91,9 @@
                     @if ($main_account == 147)
                     <li><a href="{{ route('planilla_titulacion') }}">Planilla titulación</a></li>
                     @endif
+                    @if ($main_account == 148)
                     <li><a href="{{ route('planilla_titulacion_gonvarri') }}">Planilla titulación</a></li>
+                    @endif
                     {{-- <li><a href="{{ route('dotacion_practicas') }}">Dotación y prácticas</a></li>
                     <li><a href="{{ route('verificacion_balanzas') }}">Verificación de balanzas</a></li>
                     <li><a href="{{ route('equipos_frios') }}">Temperatura equipos frios</a></li> --}}
